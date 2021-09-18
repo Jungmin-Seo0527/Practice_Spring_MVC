74ceafdccf36ddf555c3c53705bbaa021a056c30
Jungmin-Seo0527 <soato1405@naver.com>
2021-09-18 16:21:23 +0900

    HTTP 요청 데이터 - API 메시지 바디 - JSON

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StreamUtils;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
